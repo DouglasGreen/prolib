6b173d08223c7b58c4cd9f0cb2b4b6b9a9139c0f
Douglas S. Green <douglas@nurd.site>
2022-03-16 07:40:53 -0400

    Adding lexer

---
+++ lexer.pl
@@ -0,0 +1,49 @@
+/** <module> Lexer to produce labeled tokens */
+
+:- module(lexer, [word_tokens/3]).
+
+%! word_tokens(Tokens:list)
+% Match a list of word tokens.
+word_tokens([Token|Tokens]) -->
+    word_token(Token),
+    word_tokens(Tokens).
+word_tokens(Tokens) -->
+    [S],
+    {char_type(S, space)},
+    word_tokens(Tokens).
+word_tokens([]) -->  [].
+
+%! word_token(Token:comp)
+% Match a single word token.
+word_token(word(W, upper)) -->
+    char(C, upper),
+    chars(Cs, alpha),
+    !,
+    {atom_chars(W, [C|Cs])}.
+word_token(word(W, lower)) -->
+    char(C, lower),
+    chars(Cs, alpha),
+    !,
+    {atom_chars(W, [C|Cs])}.
+word_token(number(N)) -->
+    char(D, digit),
+    chars(Ds, digit),
+    !,
+    {atom_chars(N, [D|Ds])}.
+word_token(mark(M)) -->
+    char(P, punct),
+    !,
+    {atom_chars(M, [P])}.
+
+%! char(C:char)
+% Match a single character.
+char(C, Type) -->
+    [C],
+    {char_type(C, Type)}.
+
+%! chars(Cs:list<char>)
+% Match a list of characters.
+chars([C|Cs], Type) -->
+    char(C, Type),
+    chars(Cs, Type).
+chars([], _) --> [].
