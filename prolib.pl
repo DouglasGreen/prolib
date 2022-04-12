377dc47b32f1f9070a4a6997f01727ed775c8d7c
Douglas S. Green <douglas@nurd.site>
2022-04-12 19:48:43 -0400

    Adding list counts

--- prolib.pl
+++ prolib.pl
@@ -1,27 +1,44 @@
 
 
-%! chunked(+Items, +Length, -Chunks).
+%! list_chunks(+Items, +Length, -Chunks).
 % Separate a list into chunks of the given length.
-chunked(Items, Length, [Chunk|Chunks]) :-
+list_chunks(Items, Length, [Chunk|Chunks]) :-
     Length > 0,
     length(Chunk, Length),
     append(Chunk, Rest, Items),
     Rest \= [],
     !,
-    chunked(Rest, Length, Chunks).
-chunked(Items, Length, [Items]) :-
+    list_chunks(Rest, Length, Chunks).
+list_chunks(Items, Length, [Items]) :-
     Length > 0,
     length(Items, Left),
     between(1, Length, Left).
-chunked([], _, []).
+list_chunks([], _, []).
 
-:- begin_tests(prolib).
+:- begin_tests(list_chunks).
 
-test(chunked) :-
-    \+ chunked([a, b, c], 0, _),
-    chunked([], _, []),
-    chunked([a, b, c], 1, [[a], [b], [c]]),
-    chunked([a, b, c], 2, [[a, b], [c]]),
-    chunked([a, b, c], 3, [[a, b, c]]).
+test(list_chunks) :-
+    \+ list_chunks([a, b, c], 0, _),
+    list_chunks([], _, []),
+    list_chunks([a, b, c], 1, [[a], [b], [c]]),
+    list_chunks([a, b, c], 2, [[a, b], [c]]),
+    list_chunks([a, b, c], 3, [[a, b, c]]).
 
-:- end_tests(prolib).
+:- end_tests(list_chunks).
+
+%! list_counts(+Items, -Counts)
+% Count a list by pairs.
+list_counts(Items, Counts) :-
+    msort(Items, Sorted),
+    clumped(Sorted, Counts).
+
+:- begin_tests(list_counts).
+
+test(list_counts) :-
+    list_counts([], []),
+    list_counts([a, b], [a-1, b-1]),
+    list_counts([b, a], [a-1, b-1]),
+    list_counts([a, b, a], [a-2, b-1]),
+    list_counts([a, b, a, c, b], [a-2, b-2, c-1]).
+
+:- end_tests(list_counts).
